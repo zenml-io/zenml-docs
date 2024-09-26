d416a82e6480ed058170ef5c2075e14768740cee
Hamza Tahir <htahir111@gmail.com>
2024-09-26 14:39:40 +0200

    Add case

--- _scripts/run_smoke_test.sh
+++ _scripts/run_smoke_test.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 rm mint.json && rm -rf latest && rm -rf v && rm -rf develop
 
-python _scripts/process_docs_update.py --version 0.66.0
-echo "0.66.0 broken links" > broken_links.txt
-mintlify broken-links > broken_links.txt  
+# Clear the broken_links.txt file at the start
+> broken_links.txt
 
-python _scripts/process_docs_update.py --version 0.67.0
-echo "0.67.0 broken links" > broken_links.txt
-mintlify broken-links > broken_links.txt
+process_version() {
+    version=$1
+    echo "Processing version $version"
+    python _scripts/process_docs_update.py --version $version
+    echo "=== $version broken links ===" >> broken_links.txt
+    mintlify broken-links >> broken_links.txt
+    echo "" >> broken_links.txt  # Add a blank line for readability
+}
 
-python _scripts/process_docs_update.py --version develop
-echo "develop broken links" > broken_links.txt
-mintlify broken-links > broken_links.txt
+process_version "0.66.0"
+process_version "0.67.0"
+process_version "develop"
+process_version "0.68.0"
 
-python _scripts/process_docs_update.py --version 0.66.0
-echo "0.66.0 broken links" > broken_links.txt
-mintlify broken-links > broken_links.txt
-
-python _scripts/process_docs_update.py --version 0.68.0
-echo "0.68.0 broken links" > broken_links.txt
-mintlify broken-links > broken_links.txt
+echo "Broken links check complete. Results saved in broken_links.txt"
